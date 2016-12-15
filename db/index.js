61c3ac52d35f5888b25e4a055129589ca4c4bddb
Johanna <jktchon@gmail.com>
2016-12-14 22:32:44 -0800

    Update index.js

--- db/index.js
+++ db/index.js
@@ -2,16 +2,12 @@ var Sequelize = require('sequelize');
 var db = new Sequelize('crashcourse', 'root', '');
 
 var User = db.define('User', {
-  username: { type: Sequelize.STRING, field: 'username', unique: 'username' },
+  username: Sequelize.STRING,
   password: Sequelize.STRING,
   firstname: Sequelize.STRING,
   lastname: Sequelize.STRING
-});
-
-var Test = db.define('Test', {
-  test: Sequelize.STRING,
-  category: Sequelize.STRING,
-  public: Sequelize.INTEGER
+}, {
+  // timestamps: false // this will deactivate the timestamp columns
 });
 
 var Question = db.define('Question', {
@@ -19,23 +15,17 @@ var Question = db.define('Question', {
   correct: Sequelize.STRING,
   wrong1: Sequelize.STRING,
   wrong2: Sequelize.STRING,
-  wrong3: Sequelize.STRING
+  wrong3: Sequelize.STRING,
+  categories: Sequelize.STRING,
+  testName: Sequelize.STRING
 });
 
 var Results = db.define('Results', {
+  userID: Sequelize.INTEGER,
+  category: Sequelize.STRING,
   correct: Sequelize.INTEGER,
   incorrect: Sequelize.INTEGER
 });
-
-//creates a foreign key for testId in Question Table
-Question.belongsTo(Test);
-//creates foreign keys for userId and testId in Results table
-Results.belongsTo(User);
-Results.belongsTo(Test);
-//creates an invisible UsersTests table
-//This will add methods getUsers, setUsers, addUsers to Test, and getTests, setTests and addTest to User.
-User.belongsToMany(Test, {through: 'UsersTests'});
-Test.belongsToMany(User, {through: 'UsersTests'});
 
 // If we are adding columns or otherwise changing the schema
 // we can add {force: true} inside .sync to drop the tables
@@ -46,12 +36,9 @@ User.sync()
   }, function (err) {
     console.log('An error occurred while creating the Users table:', err);
   });
-
 Question.sync();
 Results.sync();
-Test.sync();
 
 exports.User = User;
 exports.Question = Question;
 exports.Results = Results;
-exports.Test = Test;
